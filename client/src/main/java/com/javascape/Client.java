7635825fac91c3e7b1b3d1e973bed17d4c966cdb
Lincoln Smith <MrSmartyMcFarty@gmail.com>
2024-01-04 14:50:51 -0600

    Initial

---
+++ client/src/main/java/com/javascape/Client.java
@@ -0,0 +1,19 @@
+package com.javascape;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX App
+ */
+public class Client extends Application {
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+
+
+}
---
+++ client/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module com.javascape {
+    requires java.desktop;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires transitive com.google.gson;
+    requires transitive javafx.base;
+    requires transitive javafx.graphics;
+
+    opens com.javascape to com.google.gson;
+    opens com.javascape.recievers to com.google.gson;
+    opens com.javascape.chronjob to com.google.gson;
+
+    //opens com.javascape.resources;
+
+    exports com.javascape;
+    // exports com.javascape.chronjob;
+    // exports com.javascape.recievers;
+    // exports com.javascape.gsonDeserializers;
+    // exports com.javascape.menuPopups;
+    // exports com.javascape.sensors;
+}
